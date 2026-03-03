67fd297db195e5d2eb7f3272e6ece3d0cce96878
GiliAndrea <giliandrea160@gmail.com>
2026-03-03 14:04:55 +0100

    inizio del lavoro del lab2 parte sulla classe del traduttore

--- translator.py
+++ translator.py
@@ -1,18 +1,39 @@
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizio = {}
 
     def printMenu(self):
         # 1. Aggiungi nuova parola
         # 2. Cerca una traduzione
         # 3. Cerca con wildcard
         # 4. Exit
-        pass
+        print("______________________________\n")
+        print("   Translator Alien-Italian\n")
+        print("______________________________\n")
+        print("1. Aggiungi nuova parola\n")
+        print("2. Cerca una traduzione\n")
+        print("3. Cerca con wildcard\n")
+        print("4. Stampa tutto il Dizionario\n")
+        print("5. Exit\n")
+        print("______________________________\n\n")
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        pass
+        import re
+        with open(dict, 'r', encoding="utf-8") as file:
+            for riga in file:
+                riga.strip()
+                riga.lower()
+                info = riga.split(" ")
+                if re.search(r'[^a-zA-Z]', info[0]) and re.search(r'[^a-zA-Z]', info[1]):
+                    ...
+                else:
+                    if info[0] in self.dizio.keys():
+                        self.dizio[info[0]] = []
+                        self.dizio[info[0]].append(info[1])
+                    else:
+                        self.dizio[info[0]].append(info[1])
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
